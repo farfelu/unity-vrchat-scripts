6abaf47b56082417e3565504ce0d2a6de8af1ffc
farfelu <git@farfelu.eu>
2024-08-11 14:36:05 +0200

    screenshot helper

---
+++ Editor/CameraScreenshot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class CameraScreenshot : MonoBehaviour
+{
+    [MenuItem("Tools/Camera Screenshot")]
+    static void CreateCameraScreenshot()
+    {
+        ScreenCapture.CaptureScreenshot("screenshot.png", 4);
+    }
+}
